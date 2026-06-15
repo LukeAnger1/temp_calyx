5e714fd9d213ab9ab1487bef6b6e0d949564539c
Luke <anger.luke2004@gmail.com>
2026-06-15 09:18:42 -0400

    Comment: Added in comments for what I want to do

--- new_pass/src/lib.rs
+++ new_pass/src/lib.rs
@@ -1,7 +1,7 @@
 use crate::traversal::{Action, Named, VisResult, Visitor};
-use calyx_ir::{self as ir, CellType, Id, NumAttr};
-use std::collections::HashSet;
-use std::rc::Rc;
+use calyx_ir::structure;
+use calyx_ir::{self as ir, LibrarySignatures};
+use calyx_utils::math::bits_needed_for;
 
 #[derive(Default)]
 pub struct WhileToRepeat {}
@@ -16,4 +16,20 @@ impl Named for WhileToRepeat {
     }
 }
 
-impl Visitor for WhileToRepeat {}
+impl Visitor for WhileToRepeat {
+    fn finish_while(
+        &mut self,
+        s: &mut ir::While,
+        comp: &mut ir::Component,
+        ctx: &LibrarySignatures,
+        _comps: &[ir::Component],
+    ) -> VisResult {
+        // 1. Extract the condition
+
+        // 2. Check if the condition can be simplified to a set number of repeats
+
+        // 3. Replace everything as a repeat loop if possible
+
+        Ok(Action::Continue)
+    }
+}
