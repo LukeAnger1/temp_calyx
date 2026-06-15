28338a93c124a91d2b704ef9320ca9ff581429e0
Luke <anger.luke2004@gmail.com>
2026-06-15 09:50:09 -0400

    Checkpoint: Added in stage 1, tested with running, behaved as NO OP

--- new_pass/src/lib.rs
+++ new_pass/src/lib.rs
@@ -1,7 +1,5 @@
 use crate::traversal::{Action, Named, VisResult, Visitor};
-use calyx_ir::structure;
 use calyx_ir::{self as ir, LibrarySignatures};
-use calyx_utils::math::bits_needed_for;
 
 #[derive(Default)]
 pub struct WhileToRepeat {}
@@ -26,10 +24,32 @@ impl Visitor for WhileToRepeat {
     ) -> VisResult {
         // 1. Extract the condition
 
+        //   Grab conditional, continue if it does not exist
+        let tempConditional = s.cond.as_ref();
+        if tempConditional.is_none() {
+            return Ok(Action::Continue);
+        }
+        let combGroup = tempConditional.unwrap();
+
+        //   Grab the port with a read lock
+        let cond_port = s.port.borrow();
+        // NOTE: Not sure if I need this test to make sure there is an out port
+        // if cond_port.name != "out" {
+        //     return Ok(Action::Continue);
+        // }
+
+        //   The port should be the out of the while loop, this code gets the cell of the parent
+        //     EX if lt then the port is lt.out and the cell is lt
+        let cond_cell = cond_port.cell_parent();
+
+        //   Release the read lock made earlier
+        drop(cond_port);
+
         // 2. Check if the condition can be simplified to a set number of repeats
 
-        // 3. Replace everything as a repeat loop if possible
+        // 3. Replace everything as a static repeat loop if possible
 
         Ok(Action::Continue)
     }
 }
+
