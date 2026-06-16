52bbf702febc126a2ceaae74fdb0d8b471292e24
Luke <anger.luke2004@gmail.com>
2026-06-15 20:45:59 -0400

    Refactor: Added in the new pass

--- run.sh
+++ run.sh
@@ -5,3 +5,6 @@ set -e
 # Run the design, switch designs manually
 fud2 matmul_bad.futil --to dat --through icarus -s sim.data=data.json
 fud2 matmul_good.futil --to dat --through icarus -s sim.data=data.json
+
+# Added in pass to have the bad design switch to repeat
+fud2 matmul_bad.futil --to dat --through icarus -s sim.data=data.json -s "calyx.args=-p validate -p while-to-repeat -p pre-opt -p compile -p lower"
